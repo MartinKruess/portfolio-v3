d38c64ec2c2c5aed7f4c2223bdc59165445d2a74
MartinK <raikunsgamezone@gmx.de>
2025-05-22 15:51:01 +0200

    add images for skills, add neon image cube at top

--- index.js
+++ index.js
@@ -1,6 +1,51 @@
+import {
+  basic,
+  adds,
+  frontend,
+  backend,
+  db,
+  bonus,
+  tools,
+} from "./scripts/skillData.js";
+
 const ageContainer = document.querySelector(".ageContainer");
 const heute = new Date();
 const bday = new Date(1990, 2, 1);
 const rawAge = heute - bday;
 const age = Math.floor(rawAge / 31536000000);
 ageContainer.innerText = `${age}`;
+
+const basicArticle = document.querySelector(".basicArticle");
+const addsArticle = document.querySelector(".addsArticle");
+const frontendArticle = document.querySelector(".frontendArticle");
+const backendArticle = document.querySelector(".backendArticle");
+const dbArticle = document.querySelector(".dbArticle");
+const bonusArticle = document.querySelector(".bonusArticle");
+const toolArticle = document.querySelector(".toolArticle");
+
+function renderTechList(techList, container) {
+  techList.forEach((tech) => {
+    const img = document.createElement("img");
+    img.src = tech.image;
+    img.alt = tech.alt;
+    container.appendChild(img);
+  });
+}
+
+renderTechList(basic, basicArticle);
+renderTechList(adds, addsArticle);
+renderTechList(frontend, frontendArticle);
+renderTechList(backend, backendArticle);
+renderTechList(db, dbArticle);
+renderTechList(bonus, bonusArticle);
+renderTechList(tools, toolArticle);
+
+// basic.forEach((tech) => {
+//   basicArticle.insertAdjacentHTML("beforeend", `<img src="${tech.image}" alt="${tech.alt}" />`);alt} />`)
+// });
+// adds.forEach((tech) => {});
+// frontend.forEach((tech) => {});
+// backend.forEach((tech) => {});
+// db.forEach((tech) => {});
+// bonus.forEach((tech) => {});
+// tools.forEach((tech) => {});
---
+++ scripts/skillData.js
@@ -0,0 +1,213 @@
+export const basic = [
+  {
+    title: "HTML",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
+    alt: "HTML Logo",
+  },
+  {
+    title: "CSS",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg",
+    alt: "CSS Logo",
+  },
+  {
+    title: "SCSS/SASS",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sass/sass-original.svg",
+    alt: "SCSS/SASS Logo",
+  },
+  {
+    title: "Tailwind",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
+    alt: "Tailwind Logo",
+  },
+  {
+    title: "Bootstrap",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original.svg",
+    alt: "Bootstrap Logo",
+  },
+  {
+    title: "Styled Components",
+    image: "/images/techstack/styled-components.svg",
+    alt: "StyledComponents Logo",
+  },
+];
+
+export const adds = [
+  {
+    title: "Markdown",
+    image: "/images/techstack/markdown-original.svg",
+    alt: "Markdown Logo",
+  },
+  {
+    title: "Git",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg",
+    alt: "Git Logo",
+  },
+  {
+    title: "GitHub",
+    image: "./images/techstack/github-original.svg",
+    alt: "GitHub Logo",
+  },
+  {
+    title: "JSON",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/json/json-original.svg",
+    alt: "JSON Logo",
+  },
+  {
+    title: "Powershell",
+    image: "/images/techstack/powershell-original.svg",
+    alt: "Powershell Logo",
+  },
+  {
+    title: "Bash",
+    image: "/images/techstack/bash-original.svg",
+    alt: "Bash Logo",
+  },
+  {
+    title: "NPM",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/npm/npm-original-wordmark.svg",
+    alt: "NPM Logo",
+  },
+];
+
+export const frontend = [
+  {
+    title: "JavaScript",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+    alt: "JavaScript Logo",
+  },
+  {
+    title: "TypeScript",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg",
+    alt: "TypeScript Logo",
+  },
+  {
+    title: "React",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
+    alt: "React Logo",
+  },
+  {
+    title: "NextJS",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg",
+    alt: "NextJS Logo",
+  },
+  {
+    title: "ElectronJS",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/electron/electron-original.svg",
+    alt: "ElectronJS Logo",
+  },
+];
+
+export const backend = [
+  {
+    title: "NodeJS",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg",
+    alt: "NodeJS Logo",
+  },
+  {
+    title: "Express",
+    image: "./images/techstack/express-original.svg",
+    alt: "Express Logo",
+  },
+  {
+    title: "DenoJS",
+    image: "./images/techstack/denojs-original.svg",
+    alt: "DenoJS Logo",
+  },
+  {
+    title: "PHP",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/php/php-original.svg",
+    alt: "PHP Logo",
+  },
+  {
+    title: "Python",
+    image: "/images/techstack/python-original.svg",
+    alt: "Python Logo",
+  },
+];
+
+export const db = [
+  {
+    title: "MongoDB",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg",
+    alt: "MongoDB Logo",
+  },
+  {
+    title: "Mongoose",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongoose/mongoose-original.svg",
+    alt: "Mongoose Logo",
+  },
+  {
+    title: "Firebase",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/firebase/firebase-original.svg",
+    alt: "Firebase Logo",
+  },
+  {
+    title: "MySQL",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original.svg",
+    alt: "MySQL Logo",
+  },
+];
+
+export const bonus = [
+  {
+    title: "Socket IO & Websockets",
+    image: "./images/techstack/socketio-original.svg",
+    alt: "Socket IO & Websockets Logo",
+  },
+  {
+    title: "oAuth & Passport",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/oauth/oauth-original.svg",
+    alt: "oAuth & Passport Logo",
+  },
+  {
+    title: "Docker",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original.svg",
+    alt: "Docker Logo",
+  },
+];
+
+export const tools = [
+  {
+    title: "Vite",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg",
+    alt: "Vite Logo",
+  },
+  {
+    title: "Photoshop",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/photoshop/photoshop-original.svg",
+    alt: "Photoshop Logo",
+  },
+  {
+    title: "After Effects",
+    image: "/images/techstack/aftereffects-original.svg",
+    alt: "After Effects Logo",
+  },
+  {
+    title: "Unreal Engine 5",
+    image: "./images/techstack/unrealengine-original.svg",
+    alt: "Unreal Engine 5 Logo",
+  },
+];
